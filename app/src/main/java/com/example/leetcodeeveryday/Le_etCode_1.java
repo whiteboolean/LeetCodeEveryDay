40a2186ba03687484b8209cde019f758a9ca89e0
whiteboolean@gmail.com <whiteboolean@gmail.com>
2021-12-12 18:33:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/leetcodeeveryday/Le_etCode_1.java
@@ -0,0 +1,17 @@
+package com.example.leetcodeeveryday;
+
+public class Le_etCode_1 {
+
+    public static void main(String[] args) {
+
+        int[] num = new int[]{1,2,2,3,4,5};
+
+        int i = removeDuplicates(num);
+        System.out.println(i);
+    }
+
+
+    private static int removeDuplicates(int[] num){
+        return 0;
+    }
+}
